da66c3b595442d987954ed78d70a38e1ce79b224
Francis Masha <francismasha96@gmail.com>
2019-09-22 05:46:19 +0300

    feat(time-scheduler): create landing page - create landing page
    [Finishes #168682304]

--- src/pages/HomePage/index.tsx
+++ src/pages/HomePage/index.tsx
@@ -6,18 +6,37 @@ import { HomePageProps, HomePageState } from './interfaces';
 // styles
 import './HomePage.scss';
 
-export class HomePage extends React.Component<HomePageProps, HomePageState> {
-  constructor(props) {
-    super(props);
-  }
-
-  render() {
-    return (
-      <React.Fragment>
-        <h2>Welcome to Almond</h2>
-      </React.Fragment>
-    );
-  }
-}
+const HomePage: React.FunctionComponent<HomePageProps> = () => {
+  return (
+    <React.Fragment>
+      <main className="cover">
+        {/*<div className="top-component"/>*/}
+        {/*<div className="bottom-component"/>*/}
+        <section className="logo">
+          <img src="https://res.cloudinary.com/almondgreen/image/upload/v1569118232/Almond/logo1_ifvhvk.png"
+               alt="Logo"/>
+        </section>
+        <section className="home-image">
+          <div className="image-wrapper">
+            <img src="https://res.cloudinary.com/almondgreen/image/upload/v1569235553/Almond/analyze_dqyrcp.svg"
+                 alt="Almond Image"/>
+          </div>
+        </section>
+        <section id="hero">
+          <div className="hero-container">
+            <div className="hero-info">
+              <h1>We have an idea!</h1>
+              <h1>Grow hydroponically.</h1>
+              <h2>Focusing on the safe production of fresh produce.</h2>
+              <button className="mdc-button mdc-button--raised">
+                <span className="mdc-button__label">Go to dashboard</span>
+              </button>
+            </div>
+          </div>
+        </section>
+      </main>
+    </React.Fragment>
+  );
+};
 
 export default HomePage;
