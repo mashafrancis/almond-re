499fc7f556e1e4097e4aea803f032a4aa7e93571
Francis Masha <francismasha96@gmail.com>
2019-10-03 17:40:32 +0300

    feat(authentication): user social authentication - add social authentication module and thunk - add login logic - integrate with firebase database
    [Finishes #167829969]

--- src/utils/firebase.ts
+++ src/utils/firebase.ts
@@ -1,12 +1,20 @@
 import * as firebase from 'firebase';
 import 'firebase/auth';
 
-const firebaseConfiguration = {
+const firebaseConfig = {
   apiKey: 'AIzaSyCukzXnPUBXARLloFWK8_72Y4aryTegW1A',
-  authDomain: 'https://almond-re.firebaseio.com',
+  authDomain: 'almond-re.firebaseapp.com',
   databaseURL: 'https://almond-re.firebaseio.com',
-  storageBucket: 'gs://almond-re.appspot.com/',
+  projectId: 'almond-re',
+  storageBucket: 'almond-re.appspot.com',
+  messagingSenderId: '181012282167',
+  appId: '1:181012282167:web:20c6848464031cdeb278b2',
+  measurementId: 'G-MWCBWQ858C',
 };
-firebase.initializeApp(firebaseConfiguration);
+
+firebase.initializeApp(firebaseConfig);
+
+export const auth: any = firebase.auth();
+export const GoogleProvider = new firebase.auth.GoogleAuthProvider();
 
 export const firebaseDatabase = firebase.database();
