2e585ddd29ebf1850dda659b547784baff37dde7
Francis Masha <francismasha96@gmail.com>
2020-06-17 19:52:46 +0300

    feat: add mqtt library and configuration

--- src/App/index.tsx
+++ src/App/index.tsx
@@ -2,6 +2,8 @@
 import * as React from 'react';
 
 // third party libraries
+// import * as fs from 'fs';
+import * as path from 'path';
 import * as queryString from 'query-string';
 import { connect } from 'react-redux';
 import { withRouter } from 'react-router-dom';
@@ -9,6 +11,11 @@ import { compose } from 'redux';
 import { createFirestoreInstance } from "redux-firestore";
 import { ReactReduxFirebaseProvider } from "react-redux-firebase";
 import * as firebase from "firebase";
+import { Connector } from 'mqtt-hooks';
+
+const KEY = process.env.KEY as string;
+const CERT = process.env.CERT as string;
+const TRUSTED_CA = process.env.TRUSTED_CA as string;
 
 // components
 import {
@@ -96,6 +103,22 @@ export const App: React.FunctionComponent<AppProps> = (props) => {
     isFetchingUserDetails: boolean,
     isUserAuthenticated: boolean) => (isFetchingUserDetails && isUserAuthenticated);
 
+  const bufferKey = Buffer.from(KEY, 'utf-8');
+  const bufferCert = Buffer.from(CERT, 'utf-8');
+  const bufferCA = Buffer.from(TRUSTED_CA, 'utf-8');
+
+  const options = {
+    port: 8083,
+    host: process.env.MQTT_HOST,
+    user: process.env.MQTT_USER,
+    protocol: process.env.MQTT_PROTOCOL,
+    password: process.env.MQTT_PASSWORD,
+    key: bufferKey,
+    cert: bufferCert,
+    ca: bufferCA,
+    rejectUnauthorized: true
+  };
+
   const { isUserAuthenticated, isAdmin } = state;
   const {
     _id,
@@ -128,30 +151,32 @@ export const App: React.FunctionComponent<AppProps> = (props) => {
       >
         <MenuProvider>
           <ViewportProvider>
-            <ErrorBoundary>
-              <React.Fragment>
-                <SnackBar/>
-                <>
-                  {
-                    location.pathname !== '/'
-                    && isUserAuthenticated
-                  }
-                  {
-                    <React.Suspense fallback={
-                      <Fade
-                        in={props.loading === 'requesting'}
-                        style={{ transitionDelay: props.loading === 'requesting' ? '800ms' : '0ms' }}
-                        unmountOnExit
-                      >
-                        <LinearProgressBar />
-                      </Fade>
-                    }>
-                      <Routes/>
-                    </React.Suspense>
+            <Connector opts={options}>
+              <ErrorBoundary>
+                <React.Fragment>
+                  <SnackBar/>
+                  <>
+                    {
+                      location.pathname !== '/'
+                      && isUserAuthenticated
                     }
-                </>
-              </React.Fragment>
-            </ErrorBoundary>
+                    {
+                      <React.Suspense fallback={
+                        <Fade
+                          in={props.loading === 'requesting'}
+                          style={{ transitionDelay: props.loading === 'requesting' ? '800ms' : '0ms' }}
+                          unmountOnExit
+                        >
+                          <LinearProgressBar />
+                        </Fade>
+                      }>
+                        <Routes/>
+                      </React.Suspense>
+                      }
+                  </>
+                </React.Fragment>
+              </ErrorBoundary>
+            </Connector>
           </ViewportProvider>
         </MenuProvider>
       </UserContext.Provider>
